d9fec9adde501f878439adaef36abbc491bcadef
jnewell-7 <jacksonnewell07@gmail.com>
2024-12-11 21:12:16 -0700

    new domain name

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -2,7 +2,7 @@
 # Place in .platform/hooks/postdeploy directory
 
 # Define your domains
-DOMAINS="-d taxhawk.us-east-1.elasticbeanstalk.com"
+DOMAINS="-d taxhawk.us-east-1.elasticbeanstalk.com -d taxhawk.is404.net"
 
 # Run Certbot to obtain or renew certificates for both domains
 sudo certbot -n $DOMAINS --nginx --agree-tos --email jnewell7@byu.edu --expand --redirect
